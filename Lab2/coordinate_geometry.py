9bc29fab61dd00da417f8a7ea5f76082e7a0dea6
Atakan Coşar <129092501+atakancsar@users.noreply.github.com>
2026-02-27 16:58:14 +0300

    Delete Lab2 directory

--- Lab2/coordinate_geometry.py
+++
@@ -1,9 +0,0 @@
-import math
-
-x1 = int(input("enter x1: "))
-y1 = int(input("enter y1: "))
-x2 = int(input("enter x2: "))
-y2 = int(input("enter y2: "))
-
-distance = math.sqrt(math.pow(x1-x2, 2) + math.pow(y1-y2, 2))
-print(f"Distance: {distance}")
--- Lab2/time_logic.py
+++
@@ -1,4 +0,0 @@
-seconds = int(input("Enter second: "))
-minutes = seconds // 60
-hours = minutes // 60
-print(f"Hours: {hours}, Minutes: {minutes}, Seconds: {seconds}")
